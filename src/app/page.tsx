636cdd0c6ae022a73d10b277eff8481a9a576ac9
Aryan <aryanlaxmansingh22@gmail.com>
2024-11-12 01:01:29 -0500

    Fixed dark mode issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
               {/* Dropdown 1 */}
               <div className="w-full">
                 <label htmlFor="dropdown1" className="block text-white-700 mb-2">Passenger Class</label>
-                <select id="dropdown1" value={passengerClass} onChange={handlePassengerClassChange} className="cursor-pointer block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
+                <select id="dropdown1" value={passengerClass} onChange={handlePassengerClassChange} className="text-black cursor-pointer block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
                   <option value="1">1</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
@@ -76,7 +76,7 @@ export default function Home() {
               {/* Dropdown 2 */}
               <div className="w-full">
                 <label htmlFor="dropdown2" className="block text-white-700 mb-2">Sex</label>
-                <select id="dropdown2" value={gender} onChange={handleGenderChange} className="cursor-pointer block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
+                <select id="dropdown2" value={gender} onChange={handleGenderChange} className="text-black cursor-pointer block w-full px-4 py-2 bg-gray-200 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
                   <option value="male">Male</option>
                   <option value="female">Female</option>
                 </select>
