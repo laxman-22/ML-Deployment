875c462c14cc0f9ffb7bdeb323a75bba6b072233
Aryan <aryanlaxmansingh22@gmail.com>
2024-11-11 21:56:33 -0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
     };
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('http://4.248.15.105/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
